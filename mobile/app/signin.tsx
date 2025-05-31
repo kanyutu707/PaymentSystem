cfad263f5d8ba259cc4e6d234e51f65958f9b29a
kanyutu707 <kanyutualfred707@gmail.com>
2025-05-31 16:53:30 +0300

    index changes

--- mobile/app/signin.tsx
+++ mobile/app/signin.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import { useRouter } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -6,14 +6,56 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 const signin = () => {
   const router=useRouter();
   return (
-    <SafeAreaView>
-    <Pressable onPress={()=>router.navigate("./(tabs)")}>
-         <Text>index</Text>
+    <SafeAreaView style={styles.signincontainer}>
+      <View >
+        <Pressable>Sign In</Pressable>
+        <Pressable>Sign In</Pressable>
+      </View>
+      <View style={styles.signinform}>
+          <Text style={styles.signinlabel}>Email</Text>
+          <TextInput placeholder='email' style={styles.signininput}/>
+
+          <Text style={styles.signinlabel}>Password</Text>
+          <TextInput placeholder='email' style={styles.signininput}/>
+
+          <Pressable onPress={()=>router.navigate("./(tabs)")}>
+         <Text>SIGN IN</Text>
        </Pressable>
+      </View>
+    
        </SafeAreaView>
   )
 }
 
 export default signin
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  signincontainer:{
+      width:"100%",
+      height:"100%",
+      alignItems:"center",
+      justifyContent:"center",
+      backgroundColor:"white",
+     
+  },
+  signinform:{
+    backgroundColor:"white",
+    borderWidth:2,
+    borderColor:"orange",
+    width:"80%",
+    borderRadius:4,
+    padding:3
+  },
+  signinlabel:{
+    marginBottom:5,
+    fontWeight:700,
+    marginTop:7
+  },
+  signininput:{
+    height:50,
+    marginVertical:10,
+    width:"98%",
+    borderWidth:1,
+    borderColor:"orange"
+  }
+})
